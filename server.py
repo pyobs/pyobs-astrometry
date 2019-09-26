56a9aaab001b8f9a63f1275a576bc6ff810e8677
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-09-26 17:31:42 +0200

    cleaning up a little

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ import tempfile
 from astropy.io import fits
 from astropy.io.fits import table_to_hdu
 from flask import Flask, escape, request, Response
-from werkzeug.exceptions import HTTPException
 from astropy.table import Table
 import json
 
@@ -32,6 +31,7 @@ def astrometry():
     if 'ra' not in data or 'dec' not in data:
         return send_error('Either RA or Dec not found in request.')
 
+    # define command
     cmd = '--crpix-center --no-verify --no-tweak ' \
           ' --radius 2.0 --ra {ra} --dec {dec} --guess-scale ' \
           '--scale-units arcsecperpix --scale-low {scale_low} --scale-high {scale_high} ' \
@@ -40,18 +40,16 @@ def astrometry():
           '--solved none --match none --rdls none --wcs wcs.fits --corr none --overwrite ' \
           '-X X -Y Y -s FLUX --width {nx} --height {ny} cat.fits'
 
-
+    # and format it
     command = cmd.format(ra=data['ra'], dec=data['dec'], scale_low=data['scale_low'], scale_high=data['scale_high'],
                          nx=data['nx'], ny=data['ny'])
 
+    # solve-field executable
     exec = '/usr/local/astrometry/bin/solve-field'
     path = os.path.abspath(os.path.join(exec, '../../lib/python'))
 
     # create table
-    x = [c['x'] for c in data['cat']]
-    y = [c['y'] for c in data['cat']]
-    flux = [c['flux'] for c in data['cat']]
-    tbl = Table([x, y, flux], names=('x', 'y', 'flux'))
+    tbl = Table([data['x'], data['y'], data['flux']], names=('x', 'y', 'flux'))
 
     # create temp directory
     with tempfile.TemporaryDirectory() as tmpdir:
@@ -60,14 +58,13 @@ def astrometry():
 
         # run astrometry.net
         try:
-            output = subprocess.check_output([exec] + shlex.split(command), cwd=tmpdir, env={'PYTHONPATH': path})
-            print(output)
+            subprocess.check_output([exec] + shlex.split(command), cwd=tmpdir, env={'PYTHONPATH': path})
         except subprocess.CalledProcessError:
-            return {'error': 'Astrometry.net threw an error.'}
+            return send_error('Astrometry.net threw an error.')
 
         # WCS file exists?
         if not os.path.exists(os.path.join(tmpdir, 'wcs.fits')):
-            return {'error': 'Could not find WCS file.'}
+            return send_error('Could not find WCS file.')
 
         # open WCS file
         wcs_header = fits.getheader(os.path.join(tmpdir, 'wcs.fits'))
@@ -76,7 +73,7 @@ def astrometry():
         keywords = ['CTYPE1', 'CTYPE2', 'CRPIX1', 'CRPIX2', 'CRVAL1', 'CRVAL2', 'CD1_1', 'CD1_2', 'CD2_1', 'CD2_2']
         header = {k: wcs_header[k] for k in keywords}
 
-    response = Response(json.dumps({'success': True, 'header': header}))
+    # define response
+    response = Response(json.dumps(header))
     response.content_type = 'application/json'
     return response
-    #return {'success': True, 'header': header}
